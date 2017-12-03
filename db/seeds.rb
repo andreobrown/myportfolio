f4d8f7ccbba4c045ce2dc61512d688d5e9a6bf61
André O. Brown <andre@aob.com.jm>
2017-12-02 23:32:03 -0500

    Implemented custom scopes for portfolio items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum lacinia magna ac cursus. Sed sodales vestibulum justo nec commodo. Proin facilisis facilisis arcu, et interdum risus cursus in. In aliquet est id placerat sodales. Sed aliquam in sem in suscipit. Nullam semper imperdiet elementum. Aenean sed mauris nunc. Nam vulputate, magna non dignissim maximus, nulla ante pretium ex, eu placerat mauris diam sed ex. Sed id ullamcorper elit, imperdiet ultrices orci. Phasellus euismod sem condimentum magna sagittis, in ultrices purus varius. Integer eleifend sem non erat scelerisque sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras magna quam, ultricies non felis quis, congue dignissim sapien."
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum lacinia magna ac cursus. Sed sodales vestibulum justo nec commodo. Proin facilisis facilisis arcu, et interdum risus cursus in. In aliquet est id placerat sodales. Sed aliquam in sem in suscipit. Nullam semper imperdiet elementum. Aenean sed mauris nunc. Nam vulputate, magna non dignissim maximus, nulla ante pretium ex, eu placerat mauris diam sed ex. Sed id ullamcorper elit, imperdiet ultrices orci. Phasellus euismod sem condimentum magna sagittis, in ultrices purus varius. Integer eleifend sem non erat scelerisque sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras magna quam, ultricies non felis quis, congue dignissim sapien.",
+        topic_id: Topic.last.id
     )
 end
 
@@ -23,10 +25,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My great service",
+        subtitle: "Ruby on Rails",
         body: "Praesent feugiat aliquam varius. Nullam blandit odio sit amet vulputate egestas. Sed erat felis, ultricies in vehicula a, gravida a erat. Proin eu nulla leo. Nullam pulvinar molestie elit ac egestas. Quisque nisl felis, volutpat eget congue eget, scelerisque quis augue. Nullam vitae finibus turpis. Donec enim mi, pulvinar vel nisi vel, ornare ultrices ligula.",
         main_image: "http://via.placeholder.com/600x400",
         thumb_image: "http://via.placeholder.com/350x200"
@@ -35,3 +37,16 @@ puts "5 skills created"
 end
 
 puts "9 portolio items created"
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
+        body: "Praesent feugiat aliquam varius. Nullam blandit odio sit amet vulputate egestas. Sed erat felis, ultricies in vehicula a, gravida a erat. Proin eu nulla leo. Nullam pulvinar molestie elit ac egestas. Quisque nisl felis, volutpat eget congue eget, scelerisque quis augue. Nullam vitae finibus turpis. Donec enim mi, pulvinar vel nisi vel, ornare ultrices ligula.",
+        main_image: "http://via.placeholder.com/600x400",
+        thumb_image: "http://via.placeholder.com/350x200"
+    )
+
+end
+
+puts "1 portolio item created"
