251cada9a4d32566e747c49c0a017f9c3e7d9f8a
André O. Brown <andre@aob.com.jm>
2018-03-06 21:06:01 -0500

    Refactored alerts into helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,3 +59,15 @@ module ApplicationHelper
         "active" if current_page? path
     end
 end
+
+def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+        alert_generator alert
+    end
+end
+
+def alert_generator msg
+    js add_gritter(msg, title: "André O. Brown Portfolio")
+end
