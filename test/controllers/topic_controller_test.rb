09bc64c9d2fe843f2f20620d5f74c2a2c2a71b08
André O. Brown <andre@aob.com.jm>
2018-06-25 01:38:20 -0400

    Added test for Topic index page. Moved test Topic creation to setup method.
    Need to rename topic Scope.
    
    Added test to ensure refactoring doesn't break Topic index page.
    Refactored Topic creation to be DRY.

--- test/controllers/topic_controller_test.rb
+++ test/controllers/topic_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class TopicControllerTest < ActionDispatch::IntegrationTest
-  test "should get topic" do
+  def setup
     create_list(:topic, 5)
+  end
+  
+  test "should get index" do
+    get topics_url
+    assert_response :success
+  end
+  
+  test "should get topic" do
     get topic_url Topic.first
     assert_response :success
   end
